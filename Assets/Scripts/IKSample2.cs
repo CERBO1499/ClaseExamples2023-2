543651491e770bf021c25e6d1e901b5458a053f8
Edu Rojas <crojas@kongregate.com>
2023-09-12 11:45:51 -0500

    IK sample

--- Assets/Scripts/IKSample2.cs
+++ Assets/Scripts/IKSample2.cs
@@ -1,12 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class IKSample2 : MonoBehaviour
 {
-    [SerializeField] private float forwardForce = 0.2f;
     private Animator _animator;
 
+    [Range(0, 1f)] public float distanceToGround;
+    [SerializeField] private LayerMask nonPlayer;
     void Start()
     {
         _animator = GetComponent<Animator>();
@@ -17,6 +20,45 @@ public class IKSample2 : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.W))
         {
             _animator.SetBool("walk", !_animator.GetBool("walk"));
+        }
+    }
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+        if (_animator)
+        {
+            //Left Foot
+            _animator.SetIKPositionWeight(AvatarIKGoal.LeftFoot,1f);
+            _animator.SetIKRotationWeight(AvatarIKGoal.LeftFoot,1f);
+
+            RaycastHit hit;
+            Ray ray = new Ray(_animator.GetIKPosition(AvatarIKGoal.LeftFoot) + Vector3.up, Vector3.down);
+            if (Physics.Raycast(ray, out hit, distanceToGround + 1f,nonPlayer))
+            {
+                if (hit.transform.tag ==("Walkable"))
+                {
+                    Vector3 footposition = hit.point;
+                    footposition.y += distanceToGround;
+                    _animator.SetIKPosition(AvatarIKGoal.LeftFoot,footposition);
+                    _animator.SetIKRotation(AvatarIKGoal.LeftFoot,Quaternion.LookRotation(transform.forward,hit.normal));
+                }
+            }
+            
+            //Rigth Foot
+            _animator.SetIKPositionWeight(AvatarIKGoal.RightFoot,1f);
+            _animator.SetIKRotationWeight(AvatarIKGoal.RightFoot,1f);
+
+           ray = new Ray(_animator.GetIKPosition(AvatarIKGoal.RightFoot) + Vector3.up, Vector3.down);
+            if (Physics.Raycast(ray, out hit, distanceToGround + 1f,nonPlayer))
+            {
+                if (hit.transform.tag == ("Walkable"))
+                {
+                    Vector3 footposition = hit.point;
+                    footposition.y += distanceToGround;
+                    _animator.SetIKPosition(AvatarIKGoal.RightFoot,footposition);
+                    _animator.SetIKRotation(AvatarIKGoal.RightFoot,Quaternion.LookRotation(transform.forward,hit.normal));
+                }
+            }
         }
     }
 }
